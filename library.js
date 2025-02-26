5ce3a99a34e26088ffb0847c443bdd9618746e23
“Aneesh” <30018808+aneesh199608@users.noreply.github.com>
2025-02-26 17:11:40 +0530

    feat: implement delete logic and add styling to it

--- library.js
+++ library.js
@@ -11,7 +11,7 @@ function Book(title, author, pages, status) {
     this.status = status;
 }
 
-Book.prototype.displayName = function() {
+Book.prototype.displayName = function(index) {
     
     
     const bookDiv = document.createElement('div');
@@ -43,6 +43,10 @@ Book.prototype.displayName = function() {
     bookPages.title=this.pages;
     bookDiv.appendChild(bookPages);
 
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('button-container');
+    bookDiv.appendChild(buttonContainer);
+    
     const bookStatus = document.createElement('select');
     bookStatus.classList.add('book-status');
     bookStatus.classList.add(`status-${this.status.toLowerCase().replace(' ','-')}`);
@@ -58,15 +62,30 @@ Book.prototype.displayName = function() {
         bookStatus.className = 'book-status';
         bookStatus.classList.add(`status-${this.status.toLowerCase().replace(' ','-')}`);
     });
-    bookDiv.appendChild(bookStatus);
+    buttonContainer.appendChild(bookStatus);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 24 24">
+<path d="M 10 2 L 9 3 L 4 3 L 4 5 L 5 5 L 5 20 C 5 20.522222 5.1913289 21.05461 5.5683594 21.431641 C 5.9453899 21.808671 6.4777778 22 7 22 L 17 22 C 17.522222 22 18.05461 21.808671 18.431641 21.431641 C 18.808671 21.05461 19 20.522222 19 20 L 19 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 7 5 L 17 5 L 17 20 L 7 20 L 7 5 z M 9 7 L 9 18 L 11 18 L 11 7 L 9 7 z M 13 7 L 13 18 L 15 18 L 15 7 L 13 7 z"></path>
+</svg>`;
+    buttonContainer.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener("click", () => {
+        const bookIndex = myLibrary.findIndex(b => b.title === this.title && b.author === this.author);
+        if (bookIndex > -1) {
+            myLibrary.splice(bookIndex, 1);
+            displayLibrary();
+        }
+    })
 
     libraryContainer.appendChild(bookDiv);
 }
 
 function displayLibrary() {
     libraryContainer.innerHTML = '';
-    myLibrary.forEach (book => {
-        book.displayName();
+    myLibrary.forEach ((book, index) => {
+        book.displayName(index);
     });
 }
 
@@ -75,8 +94,8 @@ myLibrary.push(book1);
 
 const book2 = new Book('Sapiens: A Brief History of Humankind', 'Yuval Noah Harari', 318, 'Not Started' );
 myLibrary.push(book2);
-const book3 = new Book('Sapiens: A Brief History of Humankind', 'Yuval Noah Harari', 320, 'Not Started' );
-myLibrary.push(book2);
+// const book3 = new Book('Sapiens: A Brief History of Humankind', 'Yuval Noah Harari', 120, 'Not Started' );
+// myLibrary.push(book3);
 
 addButton.addEventListener('click', () => {
     bookForm.showModal();
